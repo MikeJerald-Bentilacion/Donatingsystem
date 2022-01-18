d65db8a767bc84943dd491d24434f7bd84c0028c
Jonathan Tinoy <80259364+jonathan2019-6184@users.noreply.github.com>
2022-01-18 19:22:43 +0800

    Add files via upload

---
+++ main/index.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>E-Maginhawa</title>
+    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+    <link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="../css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <link href="../css/ionicons.min.css" rel="stylesheet" type="text/css" />
+    <link href="../js/morris/morris-0.4.3.min.css" rel="stylesheet" type="text/css" />
+    <link href="../css/AdminLTE.css" rel="stylesheet" type="text/css" />
+    <link href="../css/datatables/dataTables.bootstrap.css" rel="stylesheet" type="text/css" />
+    <link href="../css/select2.css" rel="stylesheet" type="text/css" />
+    <script src="../js/jquery-1.12.3.js" type="text/javascript"></script>
+</head>
+
+<body>
+  <nav class="navbar navbar-inverse" style="border-radius:0px;">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="index.php"><img alt="Brand" src="../img/logo.png" style="width:50px; margin-top:-15px; "></a>
+      </div>
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="index.php">Home <span class="sr-only">(current)</span></a></li>
+          <li><a href="../login.php">Admin</a></li>
+          <li><a href="../pages/donor/login.php">Donor</a></li>
+          <li><a href="../pages/rider/login.php">Rider</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <div class="wrapper row-offcanvas row-offcanvas-left">
+    <div class="breadcrumb">
+      <h3>E-Maginhawa</h3>
+    </div>
+      <p style="font-size: 50px";><center>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam esse vitae sint obcaecati officia perspiciatis nihil quas neque, laboriosam maiores, rem modi expedita. Porro corporis praesentium veniam suscipit voluptatibus, deleniti, vitae obcaecati et tenetur, eligendi similique eum dolore alias sequi.</center></p>
+  </div>
+</body>
+</html>
