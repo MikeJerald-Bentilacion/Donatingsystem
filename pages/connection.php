765d777d724425806b189c6425a7b855159663f9
MikeJerald-Bentilacion <80237721+MikeJerald-Bentilacion@users.noreply.github.com>
2022-01-18 22:15:24 +0800

    Delete pages directory

--- pages/connection.php
+++
@@ -1,4 +0,0 @@
-<?php		
-	$con = mysqli_connect('localhost','root','121900','donation') or die(mysqli_error($con));
-	date_default_timezone_set("Asia/Manila");
-?>
--- pages/rider/check_username.php
+++
@@ -1,6 +0,0 @@
-<?php
-	include "../connection.php";
-	$result = mysqli_query($con,"select username from tblzone where username = '".$_POST['username']."' ");
-	$cnt = mysqli_num_rows($result);
-	print($cnt);
-?>
